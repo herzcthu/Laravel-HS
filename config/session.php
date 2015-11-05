2b8ec242cd7939b0da35f2b51446a00d397e42c2
Sithu Thwin <sithu@thwin.net>
2015-11-05 11:48:22 +0630

    change section timeout to 20 Minutes

--- config/session.php
+++ config/session.php
@@ -29,9 +29,9 @@ return [
 	|
 	*/
 
-	'lifetime' => 10,
+	'lifetime' => 20,
 
-	'expire_on_close' => false,
+	'expire_on_close' => true,
 
 	/*
 	|--------------------------------------------------------------------------
